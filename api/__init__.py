99743990c012ab606e6290e09336ea360de8c104
Damian Zawolski <260353@student.pwr.edu.pl>
2024-11-05 20:05:14 +0100

    cors updates

--- api/__init__.py
+++ api/__init__.py
@@ -1,22 +1,17 @@
 from flask import Flask
-from firebase_admin import credentials, firestore, initialize_app
-from flask_cors import CORS, cross_origin
-import firebase_admin
-from firebase_admin import credentials
-from .userAPI import userAPI
+from firebase_admin import credentials, initialize_app
+from flask_cors import CORS
 
 cred = credentials.Certificate("api/serviceAccountKey.json")
 default_app = initialize_app(cred)
-@cross_origin()
+
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'secret-key'
-    cors = CORS(app)
-    app.config['CORS_HEADERS'] = 'Content-Type'
-    
-    
-    
 
+    app.config['SECRET_KEY'] = 'secret-key'
+    CORS(app)
+    from .userAPI import userAPI
     app.register_blueprint(userAPI, url_prefix='/user')
 
     return app
